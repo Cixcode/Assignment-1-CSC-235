465b9846bc67da3094847f5134ec16a313c50c99
Cixcode <117305643+Cixcode@users.noreply.github.com>
2022-11-06 10:02:46 -0800

    Create students10.c

---
+++ students10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* This is acode to accept the input of 10 students */
+
+int main()
+{
+    int i, n=10;
+    
+    for(i=1; i<=n; i++)
+    {
+        char firstname[20];
+        printf("What is your first name?\n" );
+        scanf("%s", firstname);
+        int j;
+        for(j=1; j<=n; j++)
+        {
+          char names[] = {firstname};
+            continue;
+        }
+       
+        
+    }
+    
+     int k;
+ 
+     for(k=0; k<n; k++)
+     {
+        printf("%s\n", names[k]);
+        
+     }
+    
+    return 0;
+}
